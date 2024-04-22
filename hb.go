6a179e419d57edba5af09882b2b632ed4d8a65e3
João Lucas <jlucas.nsilva@gmail.com>
2024-04-22 11:32:57 -0300

    pre-alpha api

--- hb.go
+++ hb.go
@@ -1,6 +1,7 @@
 package hb
 
 import (
+	"html/template"
 	"io"
 	"slices"
 	"strings"
@@ -8,11 +9,20 @@ import (
 
 type (
 	Element struct {
-		Attributes [][2]string
-		Children   []Element
-		Element    string
+		children   []Element
 		attributes string
+		element    string
+		typ        int
 	}
+
+	Attributes struct {
+		attr strings.Builder
+	}
+)
+
+const (
+	typeEl = iota
+	typeText
 )
 
 var (
@@ -39,45 +49,169 @@ var (
 )
 
 func (el *Element) Render(w io.Writer) error {
+	if el.element == "" {
+		return nil
+	}
+
+	if el.typ == typeText {
+		text := template.HTMLEscapeString(el.element)
+		writeString(w, text)
+		return nil
+	}
+
+	writeString(w, "<", el.element, el.attributes)
+	if isVoid(el.element) {
+		writeString(w, " />")
+		return nil
+	}
+	writeString(w, ">")
+
+	for _, child := range el.children {
+		child.Render(w)
+	}
+
+	writeString(w, "</", el.element, ">")
 	return nil
 }
 
-func El(el, attribs string, children ...Element) Element {
+func writeString(w io.Writer, parts ...string) {
+	for _, s := range parts {
+		io.WriteString(w, s)
+	}
+}
+
+func E(el, attribs string, children ...Element) Element {
+	pad := ""
+	if attribs != "" {
+		pad = " "
+	}
+
+	res := Element{
+		element:    el,
+		children:   children,
+		attributes: pad + attribs,
+	}
+	return res
+}
+
+func Text(text string) Element {
 	return Element{
-		Element:    el,
-		attributes: attribs,
-		Children:   children,
+		element: template.HTMLEscapeString(text),
+		typ:     typeText,
 	}
 }
 
 func Lazy(el, attribs string, children ...Element) func() Element {
 	return func() Element {
-		return Element{
-			Element:    el,
-			attributes: attribs,
-			Children:   children,
-		}
+		return E(el, attribs, children...)
 	}
 }
 
-func Classes(names ...string) string {
-	return `class="` + strings.Join(names, " ") + `"`
+func If(cond bool, then Element, otherwise ...Element) Element {
+	if cond {
+		return then
+	}
+	if len(otherwise) < 1 {
+		return None
+	}
+	return otherwise[0]
 }
 
-func If(cond bool, then, otherwise func() Element) Element {
+func When(cond bool, then func() Element, otherwise ...func() Element) Element {
 	if cond {
 		return then()
 	}
-	return otherwise()
-}
-
-func When(cond bool, then func() Element) Element {
-	return If(cond, then, func() Element {
+	if len(otherwise) < 1 {
 		return None
-	})
+	}
+	return otherwise[0]()
 }
 
 func isVoid(el string) bool {
 	_, found := slices.BinarySearch(voidElements, el)
 	return found
+}
+
+func Attr(init string) *Attributes {
+	a := &Attributes{}
+	if init != "" {
+		a.write(init)
+	}
+	return a
+}
+
+func (attr *Attributes) Attr(key, value string, cond ...bool) *Attributes {
+	if key == "" {
+		return attr
+	}
+	k := template.HTMLEscapeString(key)
+
+	for _, c := range cond {
+		if !c {
+			return attr
+		}
+	}
+
+	if value == "" {
+		attr.write(k)
+		return attr
+	}
+
+	v := template.HTMLEscapeString(value)
+	attr.write(k, `="`, v, `"`)
+	return attr
+}
+
+func (attr *Attributes) Flag(key string, cond bool) *Attributes {
+	if cond {
+		attr.write(template.HTMLEscapeString(key))
+	}
+	return attr
+}
+
+func (attr *Attributes) Case(key string, m map[string]bool) *Attributes {
+	i := 0
+	attr.write(template.HTMLEscapeString(key), `="`)
+	for val, cond := range m {
+		if cond {
+			if i > 0 {
+				attr.writePlain(" ")
+			}
+			v := template.HTMLEscapeString(val)
+			attr.writePlain(v)
+			i++
+		}
+	}
+	attr.writePlain(`"`)
+	return attr
+}
+
+func (attr *Attributes) Raw(key, value string, cond ...bool) *Attributes {
+	for _, c := range cond {
+		if !c {
+			return attr
+		}
+	}
+
+	attr.write(key, `="`, value, `"`)
+	return attr
+}
+
+func (attr *Attributes) String() string {
+	return attr.attr.String()
+}
+
+func (attr *Attributes) write(parts ...string) {
+	if attr.attr.Len() > 0 {
+		attr.attr.WriteString(" ")
+	}
+	for _, s := range parts {
+		attr.attr.WriteString(s)
+	}
+}
+
+func (attr *Attributes) writePlain(parts ...string) {
+	for _, s := range parts {
+		attr.attr.WriteString(s)
+	}
 }
---
+++ hb_test.go
@@ -0,0 +1,199 @@
+package hb
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type (
+	eTestCase struct {
+		label    string
+		expected string
+		el       Element
+	}
+
+	attrTestCase struct {
+		label    string
+		expected string
+		attr     *Attributes
+	}
+)
+
+func TestWriteText(t *testing.T) {
+	buf := strings.Builder{}
+	text := "Hello world"
+	el := Text(text)
+
+	assert.Nil(t, el.Render(&buf))
+	assert.Equal(t, text, buf.String())
+}
+
+func TestE(t *testing.T) {
+	testCases := []eTestCase{
+		{
+			label:    "simple case should succeed",
+			expected: `<h1>Title</h1>`,
+			el:       E("h1", "", Text("Title")),
+		},
+		{
+			label:    "void element should render correctly",
+			expected: `<img src="/img/ninja.png" alt="Ninja pic" />`,
+			el:       E("img", `src="/img/ninja.png" alt="Ninja pic"`),
+		},
+		{
+			label:    "children should render correctly",
+			expected: `<ul class="list"><li class="item">A</li><li class="item"><button onclick="bye()">Exit</button></li></ul>`,
+			el: E("ul", `class="list"`,
+				E("li", `class="item"`, Text("A")),
+				E("li", `class="item"`,
+					E("button", `onclick="bye()"`, Text("Exit")),
+				),
+			),
+		},
+		{
+			label:    "conditional elements should render correctly",
+			expected: `<div><p>Hello, world!</p></div>`,
+			el: E("div", "",
+				If(true,
+					E("p", "", Text("Hello, world!")),
+					E("h1", "", Text("Hello, world!")),
+				),
+			),
+		},
+		{
+			label:    "conditional elements should render nothing",
+			expected: `<div></div>`,
+			el: E("div", "",
+				If(false, E("p", "", Text("Hello, world!"))),
+			),
+		},
+		{
+			label:    "conditional elements should render correctly",
+			expected: `<div><h1>Hello, world!</h1></div>`,
+			el: E("div", "",
+				If(false,
+					E("p", "", Text("Hello, world!")),
+					E("h1", "", Text("Hello, world!")),
+				),
+			),
+		},
+		{
+			label:    "conditional lazy elements should render correctly",
+			expected: `<div><p>Hello, world!</p></div>`,
+			el: E("div", "",
+				When(true,
+					Lazy("p", "", Text("Hello, world!")),
+					Lazy("h1", "", Text("Hello, world!")),
+				),
+			),
+		},
+		{
+			label:    "conditional lazy elements should render nothing",
+			expected: `<div></div>`,
+			el: E("div", "",
+				When(false, Lazy("p", "", Text("Hello, world!"))),
+			),
+		},
+		{
+			label:    "conditional lazy elements should render correctly",
+			expected: `<div><h1>Hello, world!</h1></div>`,
+			el: E("div", "",
+				When(false,
+					Lazy("p", "", Text("Hello, world!")),
+					Lazy("h1", "", Text("Hello, world!")),
+				),
+			),
+		},
+	}
+
+	for i, tc := range testCases {
+		label := fmt.Sprintf("#%v - %v", i, tc.label)
+		t.Run(label, func(t *testing.T) {
+			buf := strings.Builder{}
+			err := tc.el.Render(&buf)
+
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, buf.String())
+		})
+	}
+}
+
+func TestAttr(t *testing.T) {
+	testCases := []attrTestCase{
+		{
+			label:    "should render nothing",
+			expected: "",
+			attr:     Attr(""),
+		},
+		{
+			label:    "should render flag",
+			expected: "required",
+			attr:     Attr("required"),
+		},
+		{
+			label:    "should render class",
+			expected: `class="container"`,
+			attr:     Attr(`class="container"`),
+		},
+		{
+			label:    "should render class",
+			expected: `class="container"`,
+			attr:     Attr(`class="container"`),
+		},
+		{
+			label:    "should render class and required flag",
+			expected: `class="container" required`,
+			attr: Attr(`class="container"`).
+				Flag("required", true).
+				Flag("open", false),
+		},
+		{
+			label:    "should render class and required flag",
+			expected: `class="container" required`,
+			attr: Attr("").
+				Attr("class", "container").
+				Attr("required", "", true).
+				Attr("open", "", false),
+		},
+		{
+			label:    "should escape sequence",
+			expected: `class="&lt;&gt;"`,
+			attr:     Attr("").Attr("class", "<>"),
+		},
+		{
+			label:    "should NOT escape sequence",
+			expected: `class="<>"`,
+			attr:     Attr("").Raw("class", "<>"),
+		},
+	}
+
+	for i, tc := range testCases {
+		label := fmt.Sprintf("#%v - %v", i, tc.label)
+		t.Run(label, func(t *testing.T) {
+			assert.Equal(t, tc.expected, tc.attr.String())
+		})
+	}
+}
+
+func TestCase(t *testing.T) {
+	t.Run("should render only correct classes", func(t *testing.T) {
+		attr := Attr("required").
+			Case("class", map[string]bool{
+				"container": true,
+				"w-50":      false,
+				"w-full":    true,
+				"bg-light":  false,
+				"bg-dark":   true,
+			}).
+			String()
+
+		assert.True(t, strings.Contains(attr, "container"))
+		assert.False(t, strings.Contains(attr, "w-50"))
+		assert.True(t, strings.Contains(attr, "w-full"))
+		assert.False(t, strings.Contains(attr, "bg-light"))
+		assert.True(t, strings.Contains(attr, "bg-dark"))
+	})
+}
